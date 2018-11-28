2a14a9acc100e31d33d1c74a9ce0711b451afa61
diecaptain <kranthi.nitr@gmail.com>
2018-11-28 02:46:53 -0500

    using immediate assertion

---
+++ assert_immediate.sv
@@ -0,0 +1,34 @@
+//+++++++++++++++++++++++++++++++++++++++++++++++++
+// DUT With Immediate assertions
+//+++++++++++++++++++++++++++++++++++++++++++++++++
+module assert_immediate();
+
+reg  clk, grant, request;
+time current_time;
+
+initial begin
+  clk = 0;
+  grant   = 0;
+  request = 0;
+  #4 request = 1;
+  #4 grant = 1;
+  #4 request = 0;
+  #4 $finish;
+end
+
+always #1 clk = ~clk;
+//=================================================
+// Assertion used in always block
+//=================================================
+always @ (posedge clk)
+begin
+  if (grant == 1) begin
+     CHECK_REQ_WHEN_GNT : assert (grant && request)
+        $display ("Seems to be working as expected");
+     else
+        current_time = $time;
+        #1 $error("assert failed at time %0t", current_time);
+  end
+end
+
+endmodule
