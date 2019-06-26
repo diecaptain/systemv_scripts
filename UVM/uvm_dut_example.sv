877445e2a1b5711826fea0eb10a22eb197b85b38
Kranthi K Pamarthi <kranthi.nitr@gmail.com>
2019-06-25 22:12:57 -0500

    Create uvm_dut_example.sv

---
+++ UVM/uvm_dut_example.sv
@@ -0,0 +1,10 @@
+module uvm_dut_example(dut_if_name dif);
+  import uvm_package::*;
+  
+  // Events
+  always @ (posedge dif.clock)
+  begin
+    `uvm_info("", $sformatf("DUT received portA=%b, portB=%b",
+                            dif.portA, dif.portB), UVM_MEDIUM);
+  end
+endmodule
